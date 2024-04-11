a0cdb6151ad93e2b922ff9938bdd4797eb821ee6
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-04-11 12:47:15 -0300

    webscraper com jsoup ok

--- src/main/java/com/julius/julius/controller/ScraperController.java
+++ src/main/java/com/julius/julius/controller/ScraperController.java
@@ -1,5 +1,12 @@
 package com.julius.julius.controller;
 
+import java.io.IOException;
+
+import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,59 +28,96 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ScraperController {
 
+    private final String USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36";
+
+    // Spoof the scraping by telling the page from where the request has been sent:
+    private final String REFERRER = "https://www.google.com";
+
     private final ProdutoService produtoService;
 
     @GetMapping
-    public ProdutoScraperDTO getProduto(@RequestParam String url) {
+    public ProdutoScraperDTO getProduto(@RequestParam String url) throws IOException {
 
-        try {
+        Response response = Jsoup.connect(url)
+                .ignoreContentType(true)
+                .userAgent("Mozilla/5.0 (compatible; 008/0.83; http://www.80legs.com/webcrawler.html) Gecko/2008032620")
+                .referrer("http://www.google.com")
+                .timeout(12000)
+                .followRedirects(true)
+                .execute();
 
-            WebDriverManager.firefoxdriver().setup();
-            
-            // WebDriverManager.chromedriver().setup();
+        Document doc = response.parse();
 
-            // System.setProperty("webdriver.edge.driver", "chromedriver_win32/chromedriver");
-            // EdgeOptions options = new EdgeOptions();
+       
 
-            FirefoxOptions options = new FirefoxOptions();
-            // ChromeOptions options = new ChromeOptions();
-            System.setProperty("webdriver.firefox.bin", "/usr/bin/firefox");
-            // options.setBinary("/usr/bin/firefox");
-            // options.addArguments("--headless");
-            // options.addArguments("--no-sandbox");
-            // options.addArguments("--disable-dev-shm-usage");
-            // options.addArguments("window-size=800,600");
-            // options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+        // Configurar opções
+        // connection.userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+        // AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+        // connection.header("Accept",
+        // "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+        // connection.header("Accept-Encoding", "gzip, deflate, br");
+        // connection.header("Accept-Language", "en-US,en;q=0.5");
+        // connection.header("Connection", "keep-alive");
+        // connection.header("Referer", "https://www.google.com/");
+        // connection.header("Upgrade-Insecure-Requests", "1");
+        // connection.ignoreHttpErrors(true);
+        // connection.ignoreContentType(true);
 
-            // WebDriver driver = new EdgeDriver(options);
-            // WebDriver driver = new ChromeDriver(options);
-            WebDriver driver = new FirefoxDriver(options);
+        // Execute a requisição
+        // Connection.Response response =
+        // connection.method(Connection.Method.GET).execute();
 
-            driver.get(url);
+        // Obtenha o HTML da página
 
-            WebElement valorElemetno = driver.findElement(By.cssSelector("#productTitle"));
+        // try {
 
-            String titulo = valorElemetno.getText();
+        // WebDriverManager.firefoxdriver().setup();
 
-            String precoSalvo = "R$ ";
-            
-            valorElemetno = driver.findElement(By.className("a-price-whole"));
-            String preco = valorElemetno.getText();
-            precoSalvo += preco;
-            valorElemetno = driver.findElement(By.className("a-price-fraction"));
-            precoSalvo += ","+ valorElemetno.getText();
+        // // WebDriverManager.chromedriver().setup();
 
-            valorElemetno = driver.findElement(By.id("landingImage"));
-            String urlImagem = valorElemetno.getAttribute("src");
-            
-            driver.quit();;
+        // // System.setProperty("webdriver.edge.driver",
+        // "chromedriver_win32/chromedriver");
+        // // EdgeOptions options = new EdgeOptions();
 
-            return new ProdutoScraperDTO(titulo, precoSalvo,urlImagem);
+        // FirefoxOptions options = new FirefoxOptions();
+        // // ChromeOptions options = new ChromeOptions();
+        // System.setProperty("webdriver.firefox.bin", "/usr/bin/firefox");
+        // options.setBinary("/usr/bin/firefox");
+        // options.addArguments("--headless");
+        // options.addArguments("--no-sandbox");
+        // options.addArguments("--disable-dev-shm-usage");
+        // options.addArguments("window-size=800,600");
+        // options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+        // AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+        // // WebDriver driver = new EdgeDriver(options);
+        // // WebDriver driver = new ChromeDriver(options);
+        // WebDriver driver = new FirefoxDriver(options);
+
+        // driver.get(url);
+
+        // WebElement valorElemetno =
+        // driver.findElement(By.cssSelector("#productTitle"));
+
+        String titulo = doc.getElementById("productTitle").text();
+
+        String precoSalvo = "R$ ";
+
+        String preco = doc.getElementsByClass("a-price-whole").first().text();
+        precoSalvo += preco;
+        preco = doc.getElementsByClass("a-price-fraction").first().text();
+        precoSalvo += preco;
+
+        Element imgElement = doc.select("div#imgTagWrapperId img[src]").first();
+        String urlImagem = imgElement.attr("src");
+
+
+        return new ProdutoScraperDTO(titulo, precoSalvo,urlImagem);
+
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // return null;
     }
 
 }
