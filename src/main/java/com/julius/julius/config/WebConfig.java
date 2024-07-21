8bd515fbd7a85a74d71c1928118d8a05fc6519e7
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-07-21 19:05:09 -0300

    att

--- src/main/java/com/julius/julius/config/WebConfig.java
+++ src/main/java/com/julius/julius/config/WebConfig.java
@@ -1,10 +1,7 @@
 package com.julius.julius.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // @EnableWebMvc
