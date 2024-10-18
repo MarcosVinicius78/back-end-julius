94beb4e37e5592fc625808be776e06e61a767bbb
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-10-17 23:53:58 -0300

    trocando rota para pegar imagem no telegram

--- src/main/java/com/julius/julius/service/TelegramService.java
+++ src/main/java/com/julius/julius/service/TelegramService.java
@@ -15,7 +15,7 @@ public class TelegramService {
     // private final String TELEGRAM_CHAT_ID = "-1002452348897"; // Chat ID do canal/grupo OMC
     private final String TELEGRAM_CHAT_ID = "-1002017124017"; // Chat ID do canal/grupo julius
     // private final String TELEGRAM_CHAT_ID = "-1001724288449"; // Chat ID do canal/grupo Sergipe Ofertas
-    private final String API_IMAGEM = "https://sergipeofertas.com.br/api/produto/download/";
+    private final String API_IMAGEM = "https://sergipeofertas.com.br/api/produto/download-imagem-real/";
 
     public void enviarProdutoParaTelegram(MensagemRequest mensagem) {
         String apiUrl = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN;
