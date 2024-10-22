e48d57bb3c24632559b63c731949c66441f6b057
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-10-21 23:43:10 -0300

    s

--- src/main/java/com/julius/julius/service/TelegramService.java
+++ src/main/java/com/julius/julius/service/TelegramService.java
@@ -11,13 +11,13 @@ import org.springframework.http.ResponseEntity;
 public class TelegramService {
 
     private final String TELEGRAM_BOT_TOKEN = "7889228882:AAEZSaj2jveJvmC1vwkZEN1-fOdQgboFkm8";
-    private final String TELEGRAM_CHAT_ID = "-1002452348897"; // Chat ID do canal/grupo
+    // private final String TELEGRAM_CHAT_ID = "-1002452348897"; // Chat ID do canal/grupo
     // private final String TELEGRAM_CHAT_ID = "-1002452348897"; // Chat ID do canal/grupo OMC
     // private final String TELEGRAM_CHAT_ID = "-1002017124017"; // Chat ID do canal/grupo julius
-    // private final String TELEGRAM_CHAT_ID_SE = "-1001724288449"; // Chat ID do canal/grupo Sergipe Ofertas
-    // private final String TELEGRAM_CHAT_ID_OMC = "-1002108490823"; // Chat ID do canal/grupo Sergipe Ofertas
-    // private final String API_IMAGEM = "https://sergipeofertas.com.br/api/produto/download-imagem-real/";
-    private final String API_IMAGEM = "https://lojawap.vteximg.com.br/arquivos/AIRFRYOVEN_fogaca_apontando_dedo_mobile.png";
+    private final String TELEGRAM_CHAT_ID_SE = "-1001724288449"; // Chat ID do canal/grupo Sergipe Ofertas
+    private final String TELEGRAM_CHAT_ID_OMC = "-1002108490823"; // Chat ID do canal/grupo Sergipe Ofertas
+    private final String API_IMAGEM = "https://sergipeofertas.com.br/api/produto/download-imagem-real/";
+    // private final String API_IMAGEM = "https://lojawap.vteximg.com.br/arquivos/AIRFRYOVEN_fogaca_apontando_dedo_mobile.png";
 
     public void enviarProdutoParaTelegram(MensagemRequest mensagem) {
         String apiUrl = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN;
@@ -27,11 +27,11 @@ public class TelegramService {
         String photoUrl = "";
 
         if (mensagem.mensagem().contains("sergipeofertas")) {
-            photoUrl = apiUrl + "/sendPhoto?chat_id=" + TELEGRAM_CHAT_ID + "&photo="
+            photoUrl = apiUrl + "/sendPhoto?chat_id=" + TELEGRAM_CHAT_ID_SE + "&photo="
             + API_IMAGEM + mensagem.url()
             + "&caption=" + mensagem.mensagem() + "&parse_mode=Markdown";
         }else if(mensagem.mensagem().contains("ofertasmaiscupons")){
-            photoUrl = apiUrl + "/sendPhoto?chat_id=" + TELEGRAM_CHAT_ID + "&photo="
+            photoUrl = apiUrl + "/sendPhoto?chat_id=" + TELEGRAM_CHAT_ID_OMC + "&photo="
             + API_IMAGEM + mensagem.url()
             + "&caption=" + mensagem.mensagem() + "&parse_mode=Markdown";
         }
